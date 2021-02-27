5b0001bf195edc4af8d7ff4ce269fdb4876d0c9f
Megan Bryan <meganmbryan12@gmail.com>
2021-02-27 10:52:25 -0800

    Change error message for login

--- config/passport.js
+++ config/passport.js
@@ -17,16 +17,10 @@ passport.use(
           email: email
         }
       }).then(dbUser => {
-        // If there's no user with the given email
-        if (!dbUser) {
+        // If there's no user with the given email or password is wrong
+        if (!dbUser || !dbUser.validPassword(password)) {
           return done(null, false, {
-            message: "Incorrect email."
-          });
-        }
-        // If there is a user with the given email, but the password the user gives us is incorrect
-        else if (!dbUser.validPassword(password)) {
-          return done(null, false, {
-            message: "Incorrect password."
+            message: "Incorrect email or password."
           });
         }
         // If none of the above, return the user
