a6db9233e694bff2dcb25ab9cef3b1802cbcc97b
unknown <garthbhammonds@gmail.com>
2021-02-28 16:02:13 -0800

    package id is now linked to username

---
+++ public/js/packages.js
@@ -0,0 +1,19 @@
+// // prettier-ignore
+// $(document).ready(() => {
+
+//   const title = $("#");
+//   const description = $("#");
+//   const tracking = $("#");
+//   const carrier = $("#");
+
+//   function getPackages(title, description, tracking, carrier, expDelivery) {
+//     $.get("/api/shipments", {
+//     //   const shipments = await
+//     })
+//       .then(() => {
+//         window.location.reload;
+//       })
+//       .catch(handlePackageErr);
+//   }
+
+// });
