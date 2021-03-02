3fa35e3a6ba93a3cbfa43e2f3dc072fd0f99888d
unknown <garthbhammonds@gmail.com>
2021-03-01 21:39:06 -0800

    added notes to homepage.js

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -2,8 +2,15 @@ $(document).ready(() => {
   const username = localStorage.getItem("currentUser");
 
   $("#lil-uzi").attr("href", `/packages/${username}`);
+  // New package button link
   $("#newPackageBtn").attr("href", "/newpackage");
+
+  // Link for delivered tab
   $("#deliveredTab").attr("href", `/delivered/${username}`);
+
+  // Link for pending orders tab
   $("#allOrdersTab").attr("href", `/packages/${username}`);
+
+  // Link for logout tab
   $("#logOut").attr("href", "/logout");
 });
