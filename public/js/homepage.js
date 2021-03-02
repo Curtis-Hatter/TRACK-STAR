4e0812fd967d61c8d2b3744826f70b8bb1ba5bae
unknown <garthbhammonds@gmail.com>
2021-03-01 20:33:02 -0800

    routing of all orders, delivered, and logout tabs. functionality of delivered page

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -2,4 +2,7 @@ $(document).ready(() => {
   const username = localStorage.getItem("currentUser");
 
   $("#lil-uzi").attr("href", `/packages/${username}`);
+  $("#deliveredTab").attr("href", `/delivered/${username}`);
+  $("#allOrdersTab").attr("href", `/packages/${username}`);
+  $("#logOut").attr("href", "/logout");
 });
